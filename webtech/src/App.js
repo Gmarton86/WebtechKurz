cb023b65cbd76edd1f5d13a6d4e484cbe3850efd
ErikZurvalec <xzurvalec@stuba.sk>
2021-12-02 15:30:02 +0100

    Initial project

---
+++ webtech/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
