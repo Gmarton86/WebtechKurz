1995768bf2563de9c500939a9cdc5d40ab67060e
ErikZurvalec <xzurvalec@stuba.sk>
2021-12-09 19:38:02 +0100

    Builded

--- webtech/src/views/Home.js
+++ webtech/src/views/Home.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
 import Game from '../components/Game'
-import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div className="bg-gray-600 min-h-screen">
       <Header />
       <Game />
-      {/* <Footer /> */}
     </div>
   )
 }
